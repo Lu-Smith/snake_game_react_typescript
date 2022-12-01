369e2d4265e36da0c02dce480ef2ae0b308de937
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-01 14:56:52 +0000

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className='Footer'>
+        Coded by <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith</a>, open-source on <a href="https://github.com/Lu-Smith/snake_game_react_typescript" target="_blank" rel="noreferrer">GitHub</a>.
+    </div>
+)
 }
 
 export default Footer
