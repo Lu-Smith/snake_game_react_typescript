5ee63bb2f975e1b65a7d0be15226504743aa97a8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-02 18:11:43 +0000

    Added useInterval

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,20 +2,25 @@ import React, {useEffect, useState, useRef} from 'react'
 import '../styles/Main.css'
 import Snake from '../images/snake - Copy.png'
 import Apple from '../images/apple.png'
+import useInterval from './useInterval'
 
-const canvasX = 600
-const canvasY = 600
+const canvasX = 500
+const canvasY = 500
 const initialSnake = [[4,10], [4,10]]
 const initialApple = [14,10]
 const scale = 50
+const timeDelay = 100
 
 const Main = () => {
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
-  const [snake, setSnake] = useState(initialSnake)
-  const [apple, setApple] = useState(initialApple)
-  const [score, setScore] = useState(0)
-  const [gameOver, setGameOver] = useState(false)
+  const [snake, setSnake] = useState<number[][]>(initialSnake)
+  const [apple, setApple] = useState<number[]>(initialApple)
+  const [score, setScore] = useState<number>(0)
+  const [gameOver, setGameOver] = useState<boolean>(false)
+  const [delay, setDelay] = useState<number | null>(null)
+
+  useInterval(() => runGame(), delay)
 
   useEffect(() => {
     let fruit = document.getElementById('fruit') as HTMLCanvasElement
@@ -44,17 +49,16 @@ const Main = () => {
     setApple(initialApple)
     setScore(0)
     setGameOver(false)
-    
-    runGame()
+    setDelay(timeDelay)
   }
 
   return (
     <div className='Main'>
        <div className="score">
         <h2> Score: {score} </h2>
-        <h2> High Score: {score} </h2>
+        <h2> High Score: {score}</h2>
       </div>
-      <img src={Apple} id='fruit' width='90' alt="an apple illustration created by Luna Smith"/>
+      <img src={Apple} id='fruit' width='30' alt="an apple illustration created by Luna Smith"/>
       <canvas className='playArea' ref={canvasRef} width={`${canvasX}px`} height={`${canvasY}px`} />
       {gameOver && <div className='gameOver'>Game Over</div>}
           <button className='playButton' onClick={startGame}>Play<img src={Snake} width='90' alt=" a snake illustration created by Luna Smith"/></button>
