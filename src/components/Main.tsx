e89834222e6e1fc2d321826209ffd7d6e979c2eb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-02 16:38:40 +0000

    Added score to Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,8 +7,13 @@ const canvasY = 600
 const Main = () => {
   return (
     <div className='Main'>
+       <div className="score">
+        <h2> Score: </h2>
+        <h2> High Score: </h2>
+      </div>
       <canvas className='playArea' width={`${canvasX}px`} height={`${canvasY}px`} />
-      <button>Play</button>
+      <button className='playButton'>Play</button>
+     
     </div>
   )
 }
