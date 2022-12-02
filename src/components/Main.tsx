e7db34b1ed536a0247c689631e5e69a0fc197f67
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-02 17:13:54 +0000

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Main.css'
 import Snake from '../images/snake - Copy.png'
+import Apple from '../images/apple.png'
 
 const canvasX = 600
 const canvasY = 600
+const initialSnake = [[4,10], [4,10]]
+const initialApple = [14,10]
 
 const Main = () => {
+
+  const [snake, setSnake] = useState(initialSnake)
+  const [ apple, setApple] = useState(initialApple)
+  
+  const startGame = () => {
+     
+  }
+
   return (
     <div className='Main'>
        <div className="score">
         <h2> Score: </h2>
         <h2> High Score: </h2>
       </div>
+      <img src={Apple} width='90'/>
       <canvas className='playArea' width={`${canvasX}px`} height={`${canvasY}px`} />
-          <button className='playButton'>Play<img src={Snake} width='90'/></button>
+          <button className='playButton' onClick={startGame}>Play<img src={Snake} width='90'/></button>
     </div>
   )
 }
