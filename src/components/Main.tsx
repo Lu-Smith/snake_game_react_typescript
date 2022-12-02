4eed495cd53e9f57f4ac30eb62acefe0f5972e87
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-02 17:34:19 +0000

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,17 +16,20 @@ const Main = () => {
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(false)
 
+  const runGame = () => {
+    const newSnake = [...snake]
+    const newSnakeHead = [newSnake[0][0], newSnake[0][1]]
+    newSnake.unshift(newSnakeHead)
+    setSnake(newSnake)
+  }
+
   const startGame = () => {
     setSnake(initialSnake)
     setApple(initialApple)
     setScore(0)
     setGameOver(false)
     
-  
-    const newSnake = [...snake]
-    const newSnakeHead = [newSnake[0][0], newSnake[0][1]]
-    newSnake.unshift(newSnakeHead)
-    setSnake(newSnake)
+    runGame()
   }
 
   return (
@@ -35,10 +38,10 @@ const Main = () => {
         <h2> Score: {score} </h2>
         <h2> High Score: {score} </h2>
       </div>
-      <img src={Apple} width='90'/>
+      <img src={Apple} width='90' alt="an apple illustration created by Luna Smith"/>
       <canvas className='playArea' width={`${canvasX}px`} height={`${canvasY}px`} />
       {gameOver && <div className='gameOver'>Game Over</div>}
-          <button className='playButton' onClick={startGame}>Play<img src={Snake} width='90'/></button>
+          <button className='playButton' onClick={startGame}>Play<img src={Snake} width='90' alt=" a snake illustration created by Luna Smith"/></button>
     </div>
   )
 }
