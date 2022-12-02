fec858242093b5cc822c99de42fe8c8bb52b55b2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-02 16:32:57 +0000

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../styles/Main.css'
 
+const canvasX = 600
+const canvasY = 600
+
 const Main = () => {
   return (
-    <div className='Main'>Main</div>
+    <div className='Main'>
+      <canvas className='playArea' width={`${canvasX}px`} height={`${canvasY}px`}/>
+    </div>
   )
 }
 
