9b6775cc98c9c1bba4b60391f61a700df13d1d17
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-02 16:45:58 +0000

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Header.css'
+import Snake from '../images/snake.png'
 
 const Header = () => {
   return (
     <div className='Header'>
-       <span>Snake Game</span>
+       <img src={Snake} width='90'/><span>Snake Game</span>
     </div>
   )
 }
